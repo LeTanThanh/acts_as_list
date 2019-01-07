ba534a27ae36b7b16fcc7b2053a71c12736f1298
LeTanThanh <tanthanhcr95@gmail.com>
2019-01-07 19:35:11 +0700

    sort

--- app/controllers/to_do_lists_controller.rb
+++ app/controllers/to_do_lists_controller.rb
@@ -1,8 +1,22 @@
 class ToDoListsController < ApplicationController
-  before_action :load_to_do_list, only: %i|show|
+  before_action :load_to_do_list, only: %i|show sort|
 
   def show
     @to_do_items = @to_do_list.to_do_items
+  end
+
+  def sort
+    params["to-do-item"].each.with_index(1) do |id, position|
+      ToDoItem.find_by(id: id).update_attributes(position: position)
+    end
+
+    respond_to do |format|
+      format.js do
+        render json: {
+          success: true
+        }
+      end
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :to_do_lists
+  resources :to_do_lists do
+    member do
+      post :sort
+    end
+  end
 end
